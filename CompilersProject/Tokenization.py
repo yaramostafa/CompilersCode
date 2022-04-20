c2b92b9c3353a16dcb04bb9abad4c38f2a00d05d
yara <yaramostaffa500@gmail.com>
2022-04-21 01:34:00 +0200

    Scanner and parser
    Co-Authored-By: Gannah27 <89727382+Gannah27@users.noreply.github.com>

---
+++ CompilersProject/Tokenization.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import re
+
+ID = re.compile(r"[A-Za-z]+")
+NUM = re.compile(r"[0-9]+")
+Compare_OPS = re.compile(r">|<|=|>=|<=")
+assignment = re.compile(":=")
+semi_col = re.compile(";")
+tokensID = (':=', ';')
+tokensLoop = ('repeat', 'Until')
+rep = "repeat"
+repeat = re.compile(rep)
+Unt = "Until"
+Until = re.compile(Unt)
+root = tk.Tk()
+
+
+def statment_accept(code_in):
+    code_in = code_in.split()
+
+    if code_in[0] == tokensLoop[0] and len(code_in) > 1:  # if it starts with until
+        i = 1
+        while code_in[i] != tokensLoop[1] and i < len(code_in):
+            if (len(code_in) - 1) % 4 == 0 and (re.fullmatch(ID, code_in[i])) and (
+                    re.fullmatch(assignment, code_in[i + 1])) and (
+                    re.fullmatch(ID, code_in[i + 2]) or re.fullmatch(NUM, code_in[i + 2])) and (
+                    code_in[i + 3] == tokensID[1]):
+                i += 4
+
+            else:
+                break;
+        if i > len(code_in):
+            l = tk.Label(root, text = 'inValid', bg='red').grid(row=4, column=1)
+        elif (i + 4 < len(code_in)):
+            l = tk.Label(root, text='inValid', bg='red').grid(row=4, column=1)
+            return
+        elif (code_in[i] == tokensLoop[1]) and (re.fullmatch(ID, code_in[i + 1])) and (
+                (re.fullmatch(Compare_OPS, code_in[i + 2])) )and (
+                re.fullmatch(NUM, code_in[i + 3]) or re.fullmatch(ID, code_in[i + 3])) and i + 4 == len(code_in):
+            print("VALID")
+            l = tk.Label(root, text=' Valid ', bg='sea green').grid(row=4, column=1)
+
+        else:
+
+            l = tk.Label(root, text=' inValid 1', bg='red').grid(row=4, column=1)
+
+
+    else:
+
+        l = tk.Label(root, text=' inValid ', bg='red').grid(row=4, column=1)
+
+    tokens = []
+    for tok in code_in:
+        if code_in is None:
+            l=tk.Label(root, text='inValid statement', bg='red').grid(row=4,column=1)
+            return None
+        else:
+            if re.fullmatch(Unt, tok):
+                print("Keyword : ", tok)
+                tokens.append(["Keyword ", tok])
+            elif re.fullmatch(rep, tok):
+                print("keyword :", tok)
+                tokens.append(["Keyword", tok])
+            elif re.fullmatch(ID, tok):
+                print("ID :", tok)
+                tokens.append(['ID', tok])
+            elif re.fullmatch(NUM, tok):
+                print("Num :", tok)
+                tokens.append(['NUM', tok])
+            elif re.fullmatch(assignment, tok):
+                print("Assignment:", tok)
+                tokens.append(['Assignment', tok])
+            elif re.fullmatch(Compare_OPS, tok) or (tok != "=="):
+                print("Comparison:", tok)
+                tokens.append(['Comparison', tok])
+            elif re.fullmatch(semi_col, tok):
+                print("semi_col:", tok)
+                tokens.append(['semi_col', tok])
+            else:
+                l = tk.Label(root, text='inValid token ' + tok).grid(row=3, column=1)
+
+
+    return tokens
+
+
+def main():
+
+    root.title("Tokenization")
+    root.configure(bg='light blue')
+    root.geometry("1200x700")
+    img = ImageTk.PhotoImage(Image.open("Dfa.jpg"))
+    l = tk.Label(image=img).grid(row=0, column=4)
+    tk.Label(root, text="Please enter a sentence: ", font=('Times', 20), fg="Purple",bg='sky blue').grid(row=0)
+    user_input = tk.Entry(root)
+    user_input.place(x = 10, y = 370, width=300, height=40)
+    result = tk.Label(root, text='Waiting for statements...', font=('Times', 14), fg="red", bg='sky blue')
+    result.place(x=50, y=550)
+    btn = tk.Button(root, text='Tokenize', font=('Times', 12), bg='blue violet')
+    btn.config(command=lambda: result.config(text=statment_accept(user_input.get())))
+    btn.grid(row=3, column=1, sticky=tk.W, pady=4)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- CompilersProject/main.py
+++ CompilersProject/main.py
@@ -19,69 +19,87 @@ root = tk.Tk()
 def statment_accept(code_in):
     code_in = code_in.split()
 
-    if code_in[0] == tokensLoop[0] and len(code_in) > 1:  # if it starts with until
+    if code_in[0] == tokensLoop[0] and len(code_in)>1:  # if it starts with until
+        l=tk.Label(root,text="Keyword : repeat at state 2 ").grid(row=4,column=1)
         i = 1
         while code_in[i] != tokensLoop[1] and i < len(code_in):
-            if (len(code_in) - 1) % 4 == 0 and (re.fullmatch(ID, code_in[i])) and (
-                    re.fullmatch(assignment, code_in[i + 1])) and (
-                    re.fullmatch(ID, code_in[i + 2]) or re.fullmatch(NUM, code_in[i + 2])) and (
-                    code_in[i + 3] == tokensID[1]):
-                i += 4
+            if (re.fullmatch(ID, code_in[i])):
+                l=tk.Label(root,text="ID at state 3 :"+code_in[i]).grid(row=5,column=1)
+                if (i+1)>= len(code_in):
+                    l = tk.Label(root, text="invalid transition to state 4 ",bg='red').grid(row=4,column=1)
+                    return
+                if re.fullmatch(assignment, code_in[i + 1]):
+                    l = tk.Label(root, text="assignment at state 4 :  " + code_in[i + 1]).grid(row=6,column=1)
+                    if (i + 2) >= len(code_in):
+                        l = tk.Label(root, text="invalid transition to state 5 ", bg='red').grid(row=4,column=1)
+                        return
+                    if re.fullmatch(ID, code_in[i + 2]) or re.fullmatch(NUM, code_in[i + 2]):
+                        l = tk.Label(root, text=" state 5")
+                        if (i + 3) >= len(code_in):
+                            l = tk.Label(root, text="invalid transition to state 6 ", bg='red').grid(row=4,column=1)
+                            return
+                        if (code_in[i + 3] == tokensID[1]):
+                            l = tk.Label(root, text="semi colon at state 6 : " + code_in[i+3]).grid(row=7,column=1)
+
+                        else:
+                            l = tk.Label(root, text="invalid transition to state 6",bg='red').grid(row=4,column=1)
+                            return
+                    else:
+                        l = tk.Label(root, text="invalid transition to state 5", bg='red').grid(row=4,column=1)
+                        return
+                else:
+                    l = tk.Label(root, text="invalid transition to state 4", bg='red').grid(row=4,column=1)
+                    return
 
             else:
-                break;
-        if i > len(code_in):
-            l = tk.Label(root, text = 'inValid', bg='red').grid(row=4, column=1)
-        elif (i + 4 < len(code_in)):
-            l = tk.Label(root, text='inValid', bg='red').grid(row=4, column=1)
-            return
-        elif (code_in[i] == tokensLoop[1]) and (re.fullmatch(ID, code_in[i + 1])) and (
-                (re.fullmatch(Compare_OPS, code_in[i + 2])) )and (
-                re.fullmatch(NUM, code_in[i + 3]) or re.fullmatch(ID, code_in[i + 3])) and i + 4 == len(code_in):
-            print("VALID")
-            l = tk.Label(root, text=' Valid ', bg='sea green').grid(row=4, column=1)
+                l = tk.Label(root, text="invalid transition to state 3", bg='red').grid(row=4,column=1)
+                return
+            i = i + 4
+            if (i >= len(code_in)):
+                l = tk.Label(root, text="invalid transition to state 7", bg='red').grid(row=4,column=1)
+                return
+        if (code_in[i] == tokensLoop[1]):
+            l = tk.Label(root, text="Keyword : Until at state 7 ").grid(row=8,column=1)
+            if (i + 1) >= len(code_in):
+                l = tk.Label(root, text="invalid transition to state 8", bg='red').grid(row=4,column=1)
+                return
+            if (re.fullmatch(ID, code_in[i + 1])) or (re.fullmatch(NUM, code_in[i + 1])) :
+                l = tk.Label(root, text="TRANSITION TO STATE 8 : "+ code_in[i+1]).grid(row=9,column=1)
+                if (i + 2) >= len(code_in):
+                    l = tk.Label(root, text="invalid transition to state 9",bg='red').grid(row=4,column=1)
+                    return
+                if (re.fullmatch(Compare_OPS, code_in[i + 2])):
+                    l = tk.Label(root, text="TRANSITION TO STATE 9 : " + code_in[i + 2]).grid(row=10,column=1)
+                    if (i + 3) >= len(code_in):
+                        l = tk.Label(root, text="invalid transition to state 10",bg='red').grid(row=4,column=1)
+                        return
+                    if re.fullmatch(NUM, code_in[i + 3]) or re.fullmatch(ID, code_in[i + 3]):
+                        l = tk.Label(root, text="TRANSITION TO STATE 10 : " + code_in[i + 3]).grid(row=11,column=1)
+                    else:
+                        l = tk.Label(root, text="invalid transition to state 10" + code_in[i+3],bg='red').grid(row=4,column=1)
+                        return
+
+                else:
+                    l = tk.Label(root, text="invalid transition to state 9" + code_in[i+2],bg='red').grid(row=4,column=1)
+                    return
+
+            else:
+                l=tk.Label(root, text='inValid token at state 8' + code_in[i+1], bg='red').grid(row=4,column=1)
+                return
 
         else:
+            l = tk.Label(root, text=' invalid token at state 7' + code_in[i], bg='red').grid(row=4, column=1)
+            return
 
-            l = tk.Label(root, text=' inValid 1', bg='red').grid(row=4, column=1)
-
+        if ((i + 4) < len(code_in)):
+            l = tk.Label(root, text='inValid tranition to extra state', bg='red').grid(row=4, column=1)
+            return
 
     else:
 
-        l = tk.Label(root, text=' inValid ', bg='red').grid(row=4, column=1)
-
-    tokens = []
-    for tok in code_in:
-        if code_in is None:
-            l=tk.Label(root, text='inValid statement', bg='red').grid(row=4,column=1)
-            return None
-        else:
-            if re.fullmatch(Unt, tok):
-                print("Keyword : ", tok)
-                tokens.append(["Keyword ", tok])
-            elif re.fullmatch(rep, tok):
-                print("keyword :", tok)
-                tokens.append(["Keyword", tok])
-            elif re.fullmatch(ID, tok):
-                print("ID :", tok)
-                tokens.append(['ID', tok])
-            elif re.fullmatch(NUM, tok):
-                print("Num :", tok)
-                tokens.append(['NUM', tok])
-            elif re.fullmatch(assignment, tok):
-                print("Assignment:", tok)
-                tokens.append(['Assignment', tok])
-            elif re.fullmatch(Compare_OPS, tok) or (tok != "=="):
-                print("Comparison:", tok)
-                tokens.append(['Comparison', tok])
-            elif re.fullmatch(semi_col, tok):
-                print("semi_col:", tok)
-                tokens.append(['semi_col', tok])
-            else:
-                l = tk.Label(root, text='inValid token ' + tok).grid(row=3, column=1)
+        l = tk.Label(root, text=' inValid token at state 2', bg='red').grid(row=4, column=1)
 
 
-    return tokens
 
 
 def main():
@@ -93,15 +111,14 @@ def main():
     l = tk.Label(image=img).grid(row=0, column=4)
     tk.Label(root, text="Please enter a sentence: ", font=('Times', 20), fg="Purple",bg='sky blue').grid(row=0)
     user_input = tk.Entry(root)
-    user_input.place(x = 10, y = 370, width=300, height=40)
-    result = tk.Label(root, text='Waiting for statements...', font=('Times', 14), fg="red", bg='sky blue')
+    user_input.place(x=10, y=370, width=300, height=40)
+    result = tk.Label(root,bg='light blue')
     result.place(x=50, y=550)
     btn = tk.Button(root, text='Tokenize', font=('Times', 12), bg='blue violet')
     btn.config(command=lambda: result.config(text=statment_accept(user_input.get())))
     btn.grid(row=3, column=1, sticky=tk.W, pady=4)
 
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
