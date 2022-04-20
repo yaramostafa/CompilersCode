d2c64561596bc3b8002f172fb951b18145b59b45
yara <yaramostaffa500@gmail.com>
2022-04-20 20:35:42 +0200

    Update main.py
    Co-Authored-By: Gannah27 <89727382+Gannah27@users.noreply.github.com>

--- CompilersProject/main.py
+++ CompilersProject/main.py
@@ -13,50 +13,52 @@ rep = "repeat"
 repeat = re.compile(rep)
 Unt = "Until"
 Until = re.compile(Unt)
+root = tk.Tk()
 
 
 def statment_accept(code_in):
     code_in = code_in.split()
 
-    if code_in[0] == tokensLoop[0]:  # if it starts with until
+    if code_in[0] == tokensLoop[0] and len(code_in) > 1:  # if it starts with until
         i = 1
         while code_in[i] != tokensLoop[1] and i < len(code_in):
             if (len(code_in) - 1) % 4 == 0 and (re.fullmatch(ID, code_in[i])) and (
-            re.fullmatch(assignment, code_in[i + 1])) and (
+                    re.fullmatch(assignment, code_in[i + 1])) and (
                     re.fullmatch(ID, code_in[i + 2]) or re.fullmatch(NUM, code_in[i + 2])) and (
                     code_in[i + 3] == tokensID[1]):
                 i += 4
 
             else:
-                print("Invalid statement")
-                return 0
+                break;
         if i > len(code_in):
-            print("Invalid statement")
-            return 0
-        if (code_in[i] == tokensLoop[1]) and (re.fullmatch(ID, code_in[i + 1])) and (
-        re.fullmatch(Compare_OPS, code_in[i + 2])) and (
+            l = tk.Label(root, text = 'inValid', bg='red').grid(row=4, column=1)
+        elif (i + 4 < len(code_in)):
+            l = tk.Label(root, text='inValid', bg='red').grid(row=4, column=1)
+            return
+        elif (code_in[i] == tokensLoop[1]) and (re.fullmatch(ID, code_in[i + 1])) and (
+                re.fullmatch(Compare_OPS, code_in[i + 2])) and (
                 re.fullmatch(NUM, code_in[i + 3]) or re.fullmatch(ID, code_in[i + 3])) and i + 4 == len(code_in):
             print("VALID")
+            l = tk.Label(root, text=' Valid ', bg='sea green').grid(row=4, column=1)
 
         else:
 
-            print("Invalid statement")
-            return 0
+            l = tk.Label(root, text=' inValid ', bg='red').grid(row=4, column=1)
+
 
     else:
 
-        print("Invalid statement at state 1")
-        return 0
+        l = tk.Label(root, text=' inValid ', bg='red').grid(row=4, column=1)
 
     tokens = []
     for tok in code_in:
         if code_in is None:
-            print("Invalid repeat statement")
+            l=tk.Label(root, text='inValid statement', bg='red').grid(row=4,column=1)
             return None
         else:
             if re.fullmatch(Unt, tok):
-                print("Keyword :", tok)
-                tokens.append(["Keyword", tok])
+                print("Keyword : ", tok)
+                tokens.append(["Keyword ", tok])
             elif re.fullmatch(rep, tok):
                 print("keyword :", tok)
                 tokens.append(["Keyword", tok])
@@ -76,27 +78,27 @@ def statment_accept(code_in):
                 print("semi_col:", tok)
                 tokens.append(['semi_col', tok])
             else:
-                print("invalid token", tok)
-                return 0
+                l = tk.Label(root, text='inValid token ' + tok).grid(row=3, column=1)
+
 
     return tokens
 
 
 def main():
-    root = tk.Tk()
-    root.title("Tokenization")
 
+    root.title("Tokenization")
+    root.configure(bg='light blue')
+    root.geometry("1200x700")
     img = ImageTk.PhotoImage(Image.open("Dfa.jpg"))
     l = tk.Label(image=img).grid(row=0, column=4)
-
-    tk.Label(root, text="Please enter a sentence: ").grid(row=0)
+    tk.Label(root, text="Please enter a sentence: ", font=('Times', 20), fg="Purple",bg='sky blue').grid(row=0)
     user_input = tk.Entry(root)
-    user_input.grid(row=0, column=1)
-    result = tk.Label(root, text='')
-    result.grid(row=2, column=0, columnspan=2)
-    btn = tk.Button(root, text='Tokenize')
+    user_input.place(x = 10, y = 370, width=300, height=40)
+    result = tk.Label(root, text='Waiting for statements...', font=('Times', 14), fg="red", bg='sky blue')
+    result.place(x=50, y=550)
+    btn = tk.Button(root, text='Tokenize', font=('Times', 12), bg='blue violet')
     btn.config(command=lambda: result.config(text=statment_accept(user_input.get())))
-    btn.grid(row=1, column=1, sticky=tk.W, pady=4)
+    btn.grid(row=3, column=1, sticky=tk.W, pady=4)
 
     root.mainloop()
 
